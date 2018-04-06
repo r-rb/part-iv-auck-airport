762b6fa603c6fea2b105b907eefa5a27b99ddb6b
nathanjmcdougall <nathan.j.mcdougall@gmail.com>
2018-04-07 07:52:56 +1200

    New visualisation for cmd line
    Simple visualisation for the optimal schedule

--- any_order_arrivals_with_setup.jl
+++ any_order_arrivals_with_setup.jl
@@ -4,12 +4,14 @@ using JuMP, Cbc
 solver = CbcSolver()
 
 ## Parameters
-c = [2, 1]	# Cost of delays for each plane
-t = [1, 2]	# Ideal arrival time
-l = [2, 0.5] # Time spent on the runway
-set_up = [	[0 5]
-			[1 0]	] # Set-up times
-d_max = 10 # Maximum allowable delay
+c = [2, 1, 2, 4]	# Cost of delays for each plane
+t = [1, 2, 2, 3]	# Ideal arrival time
+l = [2, 0.5, 1, 2] # Time spent on the runway
+set_up = [	[0 5 2 3]
+			[1 0 2 3]
+			[2 0 3 4]
+			[2 0 4 4]	] # Set-up times: entry (p,q) is the minimum time required between the finish of plane p and the arrival of plane q
+d_max = 20 # Maximum allowable delay
 
 ## Computed values
 P = length(c) # Number of planes
@@ -37,6 +39,7 @@ m = Model(solver = solver)
 @variable(m, W[1:P,1:P] >= 0) # The smallest window of time that contains all intervals of time in which either plane p or plane q is landing.
 @variable(m, G[1:P,1:P] >= 0) # The gap between plane p ad q's landings
 @variable(m, A[1:P,1:P], Bin) # Binary matrix; entry (p,q) is equal to 1 if plane p arrives before plane q, and 0 otherwise.
+
 ## Objective
 @objective(m, Min, dot(c,d)) # Minimise the total cost of delays
 
@@ -55,7 +58,7 @@ for p = 1:P
 		if p != q
 			@constraint(m, G[p,q]+l[p]+l[q] == W[p,q]) # The window W is the time the two planes spend each on the runway, plus the gap between these planes.
 			@constraint(m, G[p,q] >= set_up[p,q].*A[p,q]) # Enforce set-up times
-			@constraint(m, A[p,q] + A[q,p] == 1) # Plane p comes before plane q, or q before p, but not both.
+			@constraint(m, A[p,q] + A[q,p] == 1) # Plane p comes before plane q, or q before p, but not both. This constraint is not strictly necessary, but it may decrease the feasible region.
 		end
 	end
 end
@@ -77,17 +80,71 @@ end
 # 				|--(L[q,p])--|
 
 # In the first case, W[p,q] >= l[p] + l[q], while in the second, that inequality is violated. Since G >= 0, this forces no overlap.
-# Notice also how W[p,q] = max(L[p,q], L[q,p]).
+# Notice also how W[p,q] = max(L[p,q], L[q,p]). At least one of the two arguments must be positive, so W must be too.
 
 ## Solve
 status = solve(m)
 
+## Get solution
+objective_value = getobjectivevalue(m)
+arrivals = getvalue(a)
+exits = getvalue(e)
+ideals = t
+
 ## Print solution
 if status == :Optimal
-	println("Objective value: ", getobjectivevalue(m))
-	println("Arrival schedule: ", getvalue(a))
-	println("Runway exit times: ", getvalue(e))
+	println("Objective value: ", objective_value)
+	println("Arrival schedule: ", arrivals)
+	println("Runway exit times: ", exits)
 else
 	println("Status: ", status)
 end
 
+## Post-processing
+event_times = sort(union(ideals,arrivals,exits))
+T = length(event_times)
+
+is_ideally_arrived = Array(Bool, P, T)
+is_arrived = Array(Bool, P, T)
+is_finished = Array(Bool, P, T)
+for p=1:P
+	is_ideally_arrived[p,:] = (event_times .>= ideals[p])
+	is_arrived[p,:] = (event_times .>= arrivals[p])
+	is_finished[p,:] = (event_times .>= exits[p])
+end
+is_delayed = is_ideally_arrived & !is_arrived
+is_landing = is_arrived & !is_finished
+
+## Display schedule
+print("\n")
+event_line = "|"
+for ev = 1:T
+	added = "--$(round(event_times[ev],1))"
+	event_line *= added * "-"^(7-length(added))
+end
+event_line *= "--|\n"
+print(event_line)
+
+for p = 1:P
+	plane_line = "|"
+	plane_land = false
+	for ev = 1:T
+		if is_landing[p,ev]
+			if plane_land
+				plane_line *= "======="
+			else
+				plane_land = true
+				plane_line *= "---<==="
+			end
+		else
+			if plane_land
+				plane_land = false
+				plane_line *= "===>---"
+			else
+				plane_line *= "-------"
+			end
+		end
+	end
+	plane_line *= "--|\n"
+	print(plane_line)
+end
