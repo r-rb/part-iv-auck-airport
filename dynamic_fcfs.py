5173493dd29510d511543887c5e823fabd563012
rayner rebello <rreb176@aucklanduni.ac.nz>
2018-08-01 23:16:13 +1200

    added dp code - states + transitions

--- dynamic_fcfs.py
+++ dynamic_fcfs.py
@@ -16,12 +16,28 @@ def dominated(state1,state2):
 def transitions(state):
     pass
 
-def valid(state,cl,r,w_max):
-    
+def valid(state,cl,r,cl_info):
 
-def set_states(cl,r,stage,w_max):
+    if state[cl] + 1 <= cl_info["max"][cl]:
+
+        targ                = cl_info["targets"][state[cl]]
+        rop                 = state["rop"][r]
+
+        t                   = max(targ,rop[1] + cl_info["sep"][rop[0]][cl])
+        
+        new_state           = state
+        new_state[cl]       += 1
+        new_state["prev"]   = rop[0]
+        new_state["rop"][r] = (cl,t)
+        new_state["cost"]   += cl_info["cost"][cl] * (t - targ)
+    
+        return t,new_state
+
+    return None,None
+    
+def get_states(cl,r,stage,cl_info):
     for state in stage:
-        valid(state,cl,r,w_max)
+        valid(state,cl,r,cl_info)
 
 def expand(stage,pl_classes,cl_max,R):
     
@@ -30,38 +46,35 @@ def expand(stage,pl_classes,cl_max,R):
     # test the landing of all classes
     for cl in pl_classes:
         for r in range(0,R):
-            set_states(cl,r,stage)
+            get_states(cl,r,stage)
 
         
 
     return new_states
 
-def prune(stage):
-    pass
-
-def targeted_time(p_class,idx):
-    pass
-
 def dp_fcfs(targ,w_class,cost,d_max = 99999,R = 1):
-
-    classes     = list(sorted(set(w_class)))                # Set of classes
-    W           = len(classes)                              # Number of classes
-    cl_max       = {k: w_class.count(k) for k in w_class}    # Number of each class
-    n_stages    = len(targ) + 1
-    stages = [[]] * n_stages
+               
+    cl_info             = {}
+    cl_info["classes"]  = list(sorted(set(w_class)))
+    cl_info["max"]      = {k: w_class.count(k) for k in w_class}
+    cl_info["total"]    = len(cl_info["classes"])
+    
+    # Number of each class
+    n_stages            = len(targ) + 1
+    stages              = [[]] * n_stages
 
     # Initial state
-    init            = {k: 0 for k in classes}
-    init["rop"]     = {i:(-1,-1) for i in range(0,R)}
-    init["cost"]    = 0
-    init["prev"]    = None
+    init                = {k: 0 for k in cl_info["classes"]}
+    init["rop"]         = {i:(-1,-1) for i in range(0,R)}
+    init["cost"]        = 0
+    init["prev"]        = None
 
     stages[0].append(init)
 
     for n,stage in enumerate(stages):
         if n < len(targ):
             new_states = expand(stage)
-            stages[n+1].extend(new_states,classes,cl_max)
+            #stages[n+1].extend(new_states,classes,cl_max)
 
             # Prune dominated states
             prune(stages[n+1])
@@ -75,7 +88,6 @@ if __name__ == '__main__':
     # Sample problem
     targets         =               [1,2,3,4,5,6,7]
     wake_classes    = ['H','M','L','L','H','M','L']
-vv
     dp_fcfs(targets,wake_classes,cost,10)
 
 
