79c6b0a0a67245f27b26b81ae103c8b12e0815af
rreb176@aucklanduni.ac.nz <rreb176@UoA.auckland.ac.nz>
2018-08-06 18:12:38 +1200

    rebased simulation onto master, cleaned up surplus files, added to .gitignore

--- Rayner_Sim/plane.py
+++ Rayner_Sim/plane.py
@@ -1,13 +1,16 @@
+import math
+import numpy as np
 class Plane:
 
-    def __init__(self,flight_id,weight_class,eta,delay_cost = 1):
-        self.flight_id = flight_id
-        self.weight_class = weight_class
-        self.delay = 0
-        self.appearance_time = None
-        self.eta = eta
-        self.done = False
-        self.delay_cost = delay_cost
+    def __init__(self,flight_id,weight_class,eta,delay_cost = 1,max_delay = 36000):
+        self.id          = flight_id
+        self.weight_class       = weight_class
+        self.delay              = 0
+        self.appearance_time    = None
+        self.eta                = eta
+        self.done               = False
+        self.delay_cost         = delay_cost
+        self.max_delay          = max_delay
 
     def update(self):
         pass
@@ -26,3 +29,25 @@ class Arrival(Plane):
         self.lng = lng
         self.lat = lat
         self.appearance_time = trail[-1]["ts"]
+
+earth_radius = 6.371e6 # metres
+
+# Convert geospatial earth coordinates to cartesian rectangular coordinates
+def earth2rect(lon, lat):
+	theta = np.deg2rad(lat)
+	phi = np.deg2rad(lon)
+	R = earth_radius
+	x = R*np.cos(theta)*np.cos(phi)
+	y = R*np.cos(theta)*np.sin(phi)
+	z = R*np.sin(theta)
+	return np.array([x,y,z])
+
+# Convert rectangular coordinates back to geospatial earth coordinates
+def rect2earth(coords):
+	x = coords[0]
+	y = coords[1]
+	z = coords[2]
+	R = earth_radius
+	lat = np.rad2deg( math.asin(z/R) )
+	lon = np.rad2deg( math.atan2(y,x) )
+	return (lat, lon)
